a033d9b22608473ec405cfcb7c1037f351037c83
mIksrigz <m.iksrigz@gmail.com>
2025-01-27 17:57:12 +0100

    version2

--- scritpt.js
+++ scritpt.js
@@ -2,10 +2,11 @@ const input1 = document.getElementById('input1');
 const input2 = document.getElementById('input2');
 const result = document.getElementById('result');
 const checkBtn = document.getElementById('check-btn');
+const answerArea = document.getElementById('answer');
 
 checkBtn.addEventListener('click', () =>{
     const value1 = parseFloat(input1.value);
     const value2 = parseFloat(input2.value);
     const answer = (value1 / value2) * 100;
-    result.textContent = `${Math.round(answer)}%`;
+    answerArea.textContent = `${Math.round(answer)}%`;
 });
